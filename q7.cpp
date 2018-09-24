4859fe29e4412cfdc856f3fcaef2efee7cc8e71f
apratimchoudhury <42172482+apratimchoudhury@users.noreply.github.com>
2018-09-24 18:45:05 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 using namespace std;
 
+//function to convert lowercase to uppercase
 char toUpper(char i)
 {
 	int a= int(i);
@@ -9,11 +10,32 @@ char toUpper(char i)
 	char c= char(b);
 	return c;
 }
+
+//function to convert uppercase to lowercase
+char toLower(char i)
+{
+	int a= int(i);
+	int b= a+32;
+	char c= char(b);
+	return c;
+}
+
+
+//driver function
 int main()
 {
 	char in;	
 	cout<<"Enter a character: ";
 	cin>>in;
-	cout<<"The uppercase of this character is: "<<toUpper(in)<<endl;
+	int charValue= int(in);
+	//condition to recognise whether uppercase or lowercase
+	if(charValue>=65 && charValue<=90)
+	{
+	    cout<<"The lowercase of this character is: "<<toLower(in)<<endl;
+	}
+	
+	else
+	    cout<<"The uppercase of this character is: "<<toUpper(in)<<endl;
+	
 	return 0;
 }
